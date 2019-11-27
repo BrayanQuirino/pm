6284616a56a1f9e7c31e9fb100d5cee21bc0575b
BrayanQuirino <dracko.bq@gmail.com>
2019-11-27 02:54:33 -0600

    ya tengo la primera vizualizacion

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 app.use('/public', express.static('public'))
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname+'/population.html'));
 });
 
 app.listen(8080);
--- public/gra.js
+++ public/gra.js
@@ -1,16 +1,20 @@
-let data =d3.csv('/public/tab.csv');
+
+let data;
+(async function(){
+    data = await d3.csv('/public/tab.csv');
+    svg.selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+        .attr('id', function(d){return d.Periodo;})
+        .attr('cx', function(d){return 20*d.i;})
+        .attr('cy', function(d){return 5*d.Total;})
+        .attr('r', function(d){return d.Total;})
+        .attr('fill', function(d){return 'rgb(125,'+d.Total*100+','+d.Total*200+',100)';})
+    .append('svg:title')
+        .text(function(d){return 'Circle: '+d.Periodo;});
+})()
 const svg= d3.select('#dona').append('svg')
             .attr('width',500)
             .attr('height',500)
             .style('background-color', 'green');
-svg.selectAll('circle')
-    .data(data)
-    .enter()
-    .append('circle')
-        .attr('periodo', function(d){return d.Periodo;})
-        .attr('cx', function(d){return d.Total;})
-        .attr('cy', function(d){return d.Total;})
-        .attr('r', function(d){return d.Total;})
-        .attr('fill', function(d){return 'gray';})
-    .append('svg: title')
-        .text(function(d){return 'Circle: '+d.periodo;});
