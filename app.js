9fbc54a2ed9bda06353cc9655344b4901ec96355
BrayanQuirino <dracko.bq@gmail.com>
2019-11-28 02:25:50 -0600

    Version en centos

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var d3= require('d3')
-var express = require('express');
 var fs = require('fs');
-var app = express();
 const path = require('path');
-
+let express = require('express'),
+  port = 443,
+  app = express();
+const httpsOptions ={
+	cert:fs.readFileSync("/etc/letsencrypt/live/cndiserv.cultura.gob.mx/fullchain.pem"),
+	key:fs.readFileSync("/etc/letsencrypt/live/cndiserv.cultura.gob.mx/privkey.pem")
+}
 app.use('/public', express.static('public'));
 //app.use('/mapprueba', express.static('public'))
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/public/map/index.html'));
 });
 app.listen(8080);
-console.log('Express server started');
+https.createServer(httpsOptions,app).listen(port,function(){
+	console.log(`Serving the ${directoryToServe} directory at https:vmonet:${port}`);	
+})
+console.log('Server ready on port ' + port);
