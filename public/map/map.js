4498618240dcf6d5acf9b27b3eb01178be837ea1
BrayanQuirino <dracko.bq@gmail.com>
2019-11-28 01:09:41 -0600

    Listo, este ya se puede entregar

--- public/map/map.js
+++ public/map/map.js
@@ -9,6 +9,7 @@
           mayorA10M = '#00261C';
     var height = 600;
     var width = 900;
+    var year=2000;
     var projection = d3.geo.mercator();
     var map = void 0;
     var mexico = void 0;
@@ -19,16 +20,16 @@
         div.style.top = event.pageY + 'px';
         div.innerHTML = d.properties.NAME_1;
     };
-    var fill = function(d){
-      return data.objects.MEX_adm1;     
-    }
     var path = d3.geo.path().projection(projection);
-
     var svg = d3.select("#map")
         .append("svg")
         .attr("width", width)
         .attr("height", height);
-
+    var title = svg.append("text")
+        .attr("class", "title")
+        .attr("dy", ".71em")
+        .attr("x","730")
+        .text(year);
     d3.json('/public/map/states.json', function(data) {
         var states = topojson.feature(data, data.objects.MEX_adm1);
         console.log('states: '+ JSON.stringify(states.features[0].properties.NAME_1));
@@ -38,53 +39,15 @@
         var s = .9 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
         var t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
         projection.scale(s).translate(t);
-
         map = svg.append('g').attr('class', 'boundary');
-        d3.csv('/public/map/map1990.csv',function(data){
+        //d3.csv('/public/map/map'+year+'.csv',function(data){
+        d3.csv('/public/map/map1990.csv', function(data){
             var year0 = d3.min(data, function(d) { return d.year; }),
-            year1 = d3.max(data, function(d) { return d.year; }),
-            year = year1;
-            console.log(year0, year1);
+            year1 = d3.max(data, function(d) { return d.year; });
             mexico = map.selectAll('path').data(states.features);
-            mexico.enter()
-            .append('path')
-            .attr('d', path)
-            .attr('fill',menorA5)
-            .on("mouseover", hover);
-            //console.log(JSON.stringify(mexico[0][0].__data__.properties.NAME_1));
-            //console.log(data.length);
-            //console.log(mexico[0].length)
-            //console.log(mexico[0][0].attributes.fill.value);
-            //mexico[0][0].attributes.fill.value=mayorA10M;
-            //console.log(mexico[0][0].__data__.properties.NAME_1);
-            //console.log(data.length);
-            for(var i =0; i<data.length; i++){
-                data[i].population=+data[i].population;
-                //console.log(data[i].population);
-                //mexico[0][i].attributes.fill.value= entre2MY4M;
-                if(data[i].population <=500000){
-                mexico[0][i].attributes.fill.value= menorA5;
-                }else if(data[i].population >500000 && data[i].population <=2000000){
-                mexico[0][i].attributes.fill.value= entre5Y2M;
-                }else if(data[i].population >2000000 && data[i].population <=4000000){
-                mexico[0][i].attributes.fill.value= entre2MY4M;
-                }else if(data[i].population >4000000 && data[i].population <=6000000){
-                mexico[0][i].attributes.fill.value= entre4MY6M;
-                }else if(data[i].population >6000000 && data[i].population <=8000000){
-                mexico[0][i].attributes.fill.value= entre6MY8M;
-                }else if(data[i].population >8000000 && data[i].population <=10000000){
-                mexico[0][i].attributes.fill.value= entre8MY10M;
-                }else if(data[i].population >10000000){
-                mexico[0][i].attributes.fill.value= mayorA10M;
-                }
-            }
             //mexico[0][0].attr('fill',mayorA10M);
-            var svg = d3.select("body").append("svg")
-            var title = svg.append("text")
-                            .attr("class", "title")
-                            .attr("dy", ".71em")
-                            .text(2000);
             window.focus();
+            update();
             d3.select(window).on("keydown", function() {
                 switch (d3.event.keyCode) {
                 case 37: year = Math.max(year0, year - 5); break;
@@ -92,12 +55,52 @@
                 }
             update();
             });
-            function update() {
+            async function update() {
+                //mexico = await map.selectAll('path').data(states.features);
+                mexico.enter()
+                .append('path')
+                .attr('d', path)
+                .attr('fill',menorA5)
+                .on("mouseover", hover);
                 if (busqueda(year)){
                     title.text(year);
                 }else{
                     return;
                 }
+                for(i =0; i<data.length; i++){
+                    data[i].population=+data[i].population;
+                    data[i].year=+data[i].year;
+                    let aux;
+                    if(data[i].year==year){
+                        if(i>31 && i<64){
+                            aux=i-32
+                        }else if(i>=64 && i<96){
+                            aux=i-64;
+                        }else if(i>=96 && i<128){
+                            aux=i-96;
+                        }else if(i>=128 && i<160){
+                            aux=i-128;
+                        }else{
+                            aux=i;
+                        }
+                        if(data[i].population <=500000){
+                            mexico[0][aux].attributes.fill.value= menorA5;
+                        }else if(data[i].population >500000 && data[i].population <=2000000){
+                            mexico[0][aux].attributes.fill.value= entre5Y2M;
+                        }else if(data[i].population >2000000 && data[i].population <=4000000){
+                            mexico[0][aux].attributes.fill.value= entre2MY4M;
+                        }else if(data[i].population >4000000 && data[i].population <=6000000){
+                            mexico[0][aux].attributes.fill.value= entre4MY6M;
+                        }else if(data[i].population >6000000 && data[i].population <=8000000){
+                            mexico[0][aux].attributes.fill.value= entre6MY8M;
+                        }else if(data[i].population >8000000 && data[i].population <=10000000){
+                            mexico[0][aux].attributes.fill.value= entre8MY10M;
+                        }else if(data[i].population >10000000){
+                            mexico[0][aux].attributes.fill.value= mayorA10M;
+                        }
+                    }
+                }
+                mexico.exit().remove();
             }
             async function busqueda(y){
                 for(var z=0; z<data.length; z++){
