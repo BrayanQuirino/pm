26593a1bb1aff2a177a1e792c3bf1ec98467bba8
Brayan Quirino <dracko.bq@gmail.com>
2019-11-28 08:47:16 +0000

    esta es la version cool en centos

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var d3= require('d3')
 var fs = require('fs');
+const https =require('https');
 const path = require('path');
 let express = require('express'),
   port = 443,
@@ -13,8 +14,8 @@ app.use('/public', express.static('public'));
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/public/map/index.html'));
 });
-app.listen(8080);
+app.listen(8180);
 https.createServer(httpsOptions,app).listen(port,function(){
-	console.log(`Serving the ${directoryToServe} directory at https:vmonet:${port}`);	
+	console.log(`Serving the client directory at https:vmonet:${port}`);	
 })
 console.log('Server ready on port ' + port);
